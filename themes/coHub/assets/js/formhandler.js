33d21a76291148b7092b4b0e31e2bd4c4020a15b
Amicale Wifi Dev <hello@amicalewifi.ch>
2022-07-20 23:21:59 +0200

    Initial commit based on StaticMania/hugo-cohub template

---
+++ themes/coHub/assets/js/formhandler.js
@@ -0,0 +1,40 @@
+window.addEventListener("DOMContentLoaded", function() {    
+  var form = document.getElementById("contact-form");
+  var button = document.getElementById("contact-form-button");
+  var status = document.getElementById("contact-form-status");
+
+  function success() {
+    form.reset();
+    button.style = "display: none ";
+    status.innerHTML = "Votre message a bien été envoyé. Nous vous contacterons sous peu.";
+  }
+
+  function error() {
+    status.innerHTML = "Désolé votre message ne peut pas être envoyé.";
+  }
+
+  // handle the form submission event
+
+  form.addEventListener("submit", function(ev) {
+    ev.preventDefault();
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+  });
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
+}
